82f7c3c59abbcf720f8cf1e6f23c0876602e4e45
Thiago Tavares <tvttavares@gmail.com>
2022-05-19 14:41:09 -0400

    Spring Scheduler

---
+++ spring-scheduler/src/main/java/com/tavares/springscheduler/SpringSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tavares.springscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedulerApplication.class, args);
+    }
+
+}
