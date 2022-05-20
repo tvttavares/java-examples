7cf15a8f6e14d4a379020d9478327a338bb3b9b4
Thiago Tavares <tvttavares@gmail.com>
2022-05-20 14:25:59 -0400

    Graphql basic sample

---
+++ graphql-basic-sample/src/main/java/com/tavares/graphqlsample/GraphqlBasicSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tavares.graphqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlBasicSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlBasicSampleApplication.class, args);
+	}
+
+}
---
+++ graphql-basic-sample/src/test/java/com/tavares/graphqlsample/GraphqlBasicSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavares.graphqlsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlBasicSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
